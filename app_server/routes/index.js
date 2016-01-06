dfee9583d3c519af0cfca817cfb923662dc815ec
Paul Boyle <pboyle17@gmail.com>
2016-01-06 01:56:23 -0600

    lkjl

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var controllerMain = require('../controllers/index');
 
+
 /* GET home page. */
 router.get('/', controllerMain.index);
-
+router.get('/infoWindow',controllerMain.infoWindow);
 module.exports = router;
