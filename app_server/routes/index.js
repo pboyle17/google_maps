d85ce6f0e03cf2965afa8f5dd166ce7e09cedce6
Paul Boyle <pboyle17@gmail.com>
2016-01-05 22:03:41 -0600

    linked bootstrap and added controller

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var controllerMain = require('../controllers/main');
+var controllerMain = require('../controllers/index');
 
 /* GET home page. */
 router.get('/', controllerMain.index);
